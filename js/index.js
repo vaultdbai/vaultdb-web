e2e5430bc99f54d41de1786a705a9c8e19f80a9b
Dev M Chechi <devmchechi@gmail.com>
2026-04-11 14:56:00 -0400

    feat: implement marketplace portal with category listings and CI/CD deployment workflow

--- js/index.js
+++ js/index.js
@@ -1,69 +1,7 @@
-// FAQ 
-const lableBox = document.getElementsByClassName('accordian_box')
-const img = document.querySelector('.label_box > img')
-// console.log(img)
-
-for(i=0; i<lableBox.length; i++){
-    lableBox[i].addEventListener('click', function(){
-       const result =  this.classList.toggle('active')
-    })
-
+// FAQ accordion (homepage + database pages)
+var lableBox = document.getElementsByClassName('accordian_box');
+for (var i = 0; i < lableBox.length; i++) {
+  lableBox[i].addEventListener('click', function () {
+    this.classList.toggle('active');
+  });
 }
-
-
-
-
-    // Navbar Responsive Code ===================
-    const menuIcon = document.querySelector('.menu')
-menuIcon.addEventListener('click',()=>{
-    document.querySelector('.side_accordian').classList.add('sidebar_active')
-
-})
-
-const closeIcon = document.querySelector('.close_icon')
-closeIcon.addEventListener('click',()=>{
-    document.querySelector('.side_accordian').classList.remove('sidebar_active')
-
-})
-
-
-
-// now toggle
-const listEle = document.querySelectorAll('.link');
-console.log(listEle)
-listEle.forEach(list => {
-        console.log(list)
-        list.onclick = function(){
-                console.log(listEle)
-                this.classList.toggle('active')    
-        
-            }
-        });
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// toggle soon
-
-        // const listEle = document.querySelectorAll('.dropdown_bar');
-        // console.log(listEle) 
-        // listEle.forEach(list =>{
-        //     list.addEventListener('click',()=>{
-        //         console.log(list)
-        // list.classList.toggle('active')
-        //   })
-        // })
-
-
